5cb5204dbaa54e1a0e198e74507a0721977042f1
Abass Zakari <abasszakari26@gmail.com>
2020-12-05 09:33:47 +0000

    initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import {RiCloseCircleLine} from 'react-icons/ri';
+import {FiEdit} from 'react-icons/fi';
+
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    })
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value);
+        setEdit({
+            id: null,
+            value: ''
+        });
+    }
+
+    if(edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
+
+    return todos.map((todo, index) => (
+        <div className = {todos.isComplete ? "todo-row complete" : "todo-row"} key={index}>
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>{todo.text}</div>
+            <div className="icons">
+                <RiCloseCircleLine className="delete-icon" onClick={() => removeTodo(todo.id)}/>
+                <FiEdit className="edit-icon" onClick={() => setEdit({id:todo.id, value: todo.text}) }/>
+            </div>
+        </div>
+    ))
+}
+
+export default Todo
